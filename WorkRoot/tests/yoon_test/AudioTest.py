a72ed312f017d200a0d90ac54032832f99c24596
MoonSongiT <MoonsongiT@gmail.com>
2024-02-01 09:56:18 +0900

    1/02-1

--- WorkRoot/tests/yoon_test/AudioTest.py
+++ WorkRoot/tests/yoon_test/AudioTest.py
@@ -9,10 +9,10 @@ def convert_to_wav(input_path, output_path):
     audio_clip.write_audiofile(output_path, codec='pcm_s16le', ffmpeg_params=['-ac', '2'])
 
 if __name__ == "__main__":
-    input_directory = './'  # m4a 파일이 있는 입력 디렉토리
-    output_directory = './변환파일/'  # 변환된 wav 파일을 저장할 출력 디렉토리
+    input_directory = "C:\\ProjectWork1\\WorkRoot\\tests\\yoon_test"       # m4a 파일이 있는 입력 디렉토리
+    output_directory = 'C:\\ProjectWork1\\WorkRoot\\tests\\yoon_test\\변환파일\\'  # 변환된 wav 파일을 저장할 출력 디렉토리
 
-    # 출력 디렉토리가 없으면 생성
+    # 출력 디렉토리가 없으면 생성cd
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
@@ -23,6 +23,7 @@ if __name__ == "__main__":
             output_path = os.path.join(output_directory, os.path.splitext(filename)[0] + ".wav")
 
             # m4a를 wav로 변환
+            print(output_path)
             convert_to_wav(input_path, output_path)
 
     print("변환 완료.")
